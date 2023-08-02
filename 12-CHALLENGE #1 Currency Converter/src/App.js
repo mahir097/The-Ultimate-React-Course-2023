75c519fcd757eaaa4815c62171df51a537e29899
Mahir <mahir.demir097@gmail.com>
2023-08-02 19:51:37 +0300

    exercises

---
+++ 12-CHALLENGE #1 Currency Converter/src/App.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+export default function App() {
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [amount, setAmount] = useState(1);
+  const [exchangeRate, setExchangeRate] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+      );
+      const data = await response.json();
+      if (fromCurrency === toCurrency) setExchangeRate(1);
+      else setExchangeRate(data.rates[toCurrency]);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, [amount, fromCurrency, toCurrency]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        disabled={isLoading}
+      />
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="TRY">TRY</option>
+      </select>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="TRY">TRY</option>
+      </select>
+      <p>
+        OUTPUT: {exchangeRate} {toCurrency}
+      </p>
+    </div>
+  );
+}
